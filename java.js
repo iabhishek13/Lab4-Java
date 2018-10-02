a6d42f02e429b1224e23b482275133f95c10115b
Abhishek <40850227+iabhishek13@users.noreply.github.com>
2018-10-02 19:29:44 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,49 +1,38 @@
-let  java4;
+let java4;
 fetch("https://my.api.mockaroo.com/users.json?key=79a35aa0&qty=3")
-  .then(function(response) {
+
+.then(function(response) 
+    {
     return response.json();
-  })
-  .then(function(myJson) {
+    })
+
+.then(function(myJson) 
+    {
     java4=myJson;
+    lab4();
+    }); 
 
-lab4();
-  
-  }); 
-
- function lab4(view){
-
-  const htmlSnippet = java4.map(abhi =>
-          `
- <div id="color" onclick="colorred();" class="email-item email-item-selected pure-g ">
-<div class="pure-u" onmouseover="change()">
- <img width="64" height="64" alt="Tilo Mitra&#x27;s avatar" class="email-avatar" src="${abhi.pic}"></div>
-
-            <div class="pure-u-3-4">
-                <h5 class="email-name">${abhi.name}</h5>
-                <h4 class="email-subject">${abhi.subject}</h4>
-                <p class="email-desc">
-                ${abhi.paragraph}
-                </p>
-            </div>
+ function lab4(view)
+ {
+   const profile = java4.map(data =>`<div class="email-item email-item-selected pure-g " id="click" onclick="colorsky();">
+   <div class="pure-u" onmouseover="change()">
+   <img width="64" height="64" alt="Tilo Mitra&#x27;s avatar" class="email-avatar" src="${data.pic}"></div>
+    <div class="pure-u-3-4">
+    <h5 class="email-name">${data.name}</h5>
+    <h4 class="email-subject">${data.subject}</h4>
+    <p class="email-desc">
+     ${data.paragraph}
+      </p>
       </div>
-</div>
-`
-      )
-          .join("");
+      </div>
+      </div>`).join("");
   
-    
-    const main = document.querySelector("#list");
-      main.innerHTML = htmlSnippet;
-  
-  }
-
- 
-
-function colorred() {
- const color=document.getElementById('color');
-
-  color.style.backgroundColor = "skyblue";
-    return false;
-
-
+const main = document.querySelector("#list");
+main.innerHTML = profile;
+}
+function colorsky()
+{
+const click=document.getElementById('click');
+click.style.backgroundColor = "skyblue";
+return false;
 }    
