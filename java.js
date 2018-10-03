5fa61c83868140feee55d656e5b745b25dc33f69
Abhishek <40850227+iabhishek13@users.noreply.github.com>
2018-10-03 18:21:02 -0400

    Update java.js

--- java.js
+++ java.js
@@ -12,16 +12,36 @@ fetch("https://my.api.mockaroo.com/users.json?key=79a35aa0&qty=3")
     lab4();
     }); 
 
+function colorsky()
+{
+const click=document.getElementById('click');
+click.style.backgroundColor = "skyblue";
+return false;
+}    
+
+function colorsky1()
+{
+const click=document.getElementById('click');
+click.style.backgroundColor = "skyblue";
+return false;
+}    
+
+function colorsky2()
+{
+const click=document.getElementById('click');
+click.style.backgroundColor = "skyblue";
+return false;
+}    
+
  function lab4(view)
-     {
+ {
    const profile = java4.map(data =>`<div class="email-item email-item-selected pure-g " id="click" onclick="colorsky();">
    <div class="pure-u" onmouseover="change()">
    <img width="64" height="64" alt="Tilo Mitra&#x27;s avatar" class="email-avatar" src="${data.pic}"></div>
     <div class="pure-u-3-4">
     <h5 class="email-name">${data.name}</h5>
     <h4 class="email-subject">${data.subject}</h4>
-    <p class="email-desc">
-     ${data.paragraph}
+    <p class="email-desc">${data.paragraph}
       </p>
       </div>
       </div>
@@ -30,9 +50,3 @@ fetch("https://my.api.mockaroo.com/users.json?key=79a35aa0&qty=3")
 const abhi = document.querySelector("#list");
 abhi.innerHTML = profile;
 }
-function colorsky()
-{
-const click=document.getElementById('click');
-click.style.backgroundColor = "skyblue";
-return false;
-}    
